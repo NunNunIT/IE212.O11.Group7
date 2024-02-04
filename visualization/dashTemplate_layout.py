49a3bf9b2b0dd41584250f7b88bfd0e75af1ae03
Nguyen Viet Kha <vietkha1975@gmail.com>
2024-02-04 10:50:13 +0700

    Update dashTemplatelayout

--- visualization/dashTemplate_layout.py
+++ visualization/dashTemplate_layout.py
@@ -10,6 +10,9 @@ import csv
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics.pairwise import haversine_distances
 from math import radians
+from haversine import haversine, Unit
+
+
 
 def create_Year_layout():
     return html.Div([
@@ -49,10 +52,11 @@ def create_IDPlace_layout():
             ),
         ], className='dropdown-item')
 
-df_result = pd.read_csv('result.csv',encoding='latin-1')
+df_result = pd.read_csv('result_HCM1.csv',encoding='utf-8')
 def create_dashtable_result():
     df_result_filtered = df_result.drop(columns=["gPlusPlaceId"])
-    df_result_filtered = df_result_filtered.drop(df_result_filtered.index[0])
+    df_result_filtered = df_result_filtered.drop(df_result.index[0])
+    print(df_result_filtered)
     return html.Div([
             html.Div([
                 dcc.Graph(id='map-result', config={'displayModeBar': False})
@@ -134,14 +138,10 @@ def update_graph(df):
                                     color_discrete_sequence=['#043296'],
                                     category_orders=dict(rating=["1", "2", "3", "4", "5"]))
     df2 = dff.copy()
-    df2['gps'] = dff['gps'].apply(lambda x: ast.literal_eval(x))
-    df2['lat'] = df2['gps'].apply(lambda x: x[0])
-    df2['lon'] = df2['gps'].apply(lambda x: x[1])
-    df2 = df2.drop(columns=['gps'])
 
     map_figure = px.scatter_mapbox(df2,
-                                   lat='lat',
-                                   lon='lon',
+                                   lat='location/lat',
+                                   lon='location/lng',
                                    text='name',
                                    mapbox_style='carto-positron',
                                    color_discrete_sequence=['#002f72'])
@@ -181,33 +181,36 @@ def find_placeid(place_name):
     return place_id
 
 # Đọc dữ liệu từ tệp CSV và xây dựng mô hình ở đây
-df1 = pd.read_csv('Predict.csv')
-
-# Chuyển định dạng của cột 'gps' sang list
-df1['gps'] = df1['gps'].apply(eval)
+data = pd.read_csv('rated_places_new.csv', encoding='utf-8')
 
 # Tiền xử lý dữ liệu
 tfidf_vectorizer = TfidfVectorizer(stop_words='english')
-tfidf_matrix = tfidf_vectorizer.fit_transform(df1['categories'].fillna(''))
+tfidf_matrix = tfidf_vectorizer.fit_transform(data['categories'].fillna(''))
 
 # Xây dựng mô hình
-model = NearestNeighbors(n_neighbors=6, algorithm='brute')
+model = NearestNeighbors(n_neighbors=6, algorithm='ball_tree')
 model.fit(tfidf_matrix)
 # Hàm khuyến nghị
 def recommend(gPlusPlaceId):
     # Lấy thông tin từ dataset dựa trên gPlusPlaceId
-    user_info = df1[df1['gPlusPlaceId'] == gPlusPlaceId][['name','categories','average_rating', 'gps','gPlusPlaceId']]
+    user_info = data[data['gPlusPlaceId'] == gPlusPlaceId][['name', 'categories', 'average_rating', 'location/lat',
+                                                          'location/lng', 'gPlusPlaceId']]
 
-    # Lấy gps từ dataset dựa trên gPlusPlaceId
-    input_gps = user_info['gps'].iloc[0]
+    # Lấy vị trí từ dataset dựa trên gPlusPlaceId
+    input_lat = data[data['gPlusPlaceId'] == gPlusPlaceId]['location/lat'].iloc[0]
+    input_lng = data[data['gPlusPlaceId'] == gPlusPlaceId]['location/lng'].iloc[0]
 
-    idx = df1[df1['gPlusPlaceId'] == gPlusPlaceId].index[0]
+    input_coords = [input_lat, input_lng]
+
+    idx = data[data['gPlusPlaceId'] == gPlusPlaceId].index[0]
     distances, indices = model.kneighbors(tfidf_matrix[idx])
-    recommended_places = df1.loc[indices[0][1:]]
+    recommended_places = data.loc[indices[0][1:]]
 
-    # Kiểm tra khoảng cách giữa gps nhập vào và gps của các địa điểm được khuyến nghị
-    input_gps_radians = [radians(coord) for coord in input_gps]
-    recommended_places['distance'] = recommended_places['gps'].apply(lambda x: haversine_distances([input_gps_radians, [radians(coord) for coord in x]])[0][0] * 6371000 / 1000.0)
+    # Kiểm tra khoảng cách giữa vị trí nhập vào và vị trí của các địa điểm được khuyến nghị
+    input_coords_radians = [radians(coord) for coord in input_coords]
+    recommended_places['distance'] = recommended_places.apply(
+        lambda row: haversine(input_coords_radians, [radians(row['location/lat']), radians(row['location/lng'])],
+                              unit=Unit.KILOMETERS), axis=1)
     recommended_places = recommended_places.sort_values(by='distance').drop(columns=['distance'])
 
     # Loại bỏ gPlusPlaceId nhập vào khỏi danh sách khuyến nghị
@@ -216,28 +219,28 @@ def recommend(gPlusPlaceId):
     # Sắp xếp theo average_rating giảm dần
     recommended_places = recommended_places.sort_values(by='average_rating', ascending=False)
 
-    return user_info, recommended_places[['name', 'categories', 'average_rating', 'gps', 'gPlusPlaceId']]
+    return user_info, recommended_places[['name','categories','average_rating','location/lat','location/lng','gPlusPlaceId']]
 
 # average_ratings = df.groupby('gPlusPlaceId')['average_rating'].mean().reset_index()
 # merged_data = pd.merge(average_ratings, df[['gPlusPlaceId', 'gps', 'categories', 'name']], on='gPlusPlaceId', how='left')
 # df1 = merged_data.drop_duplicates(subset='gPlusPlaceId', keep='first')
 
 def recommendation(placeid):
-    data = df1
+    df1 = data
     user_info, result = recommend(placeid)
     # Ghi thông tin của user_input và kết quả vào cùng một tệp CSV
-    with open('result.csv', 'w', newline='') as file:
+    with open('result_HCM1.csv', 'w', newline='', encoding='utf-8-sig') as file:
         writer = csv.writer(file)
 
         # Ghi header
-        writer.writerow(['name', 'categories', 'average_rating', 'GPS', 'gPlusPlaceId'])
+        writer.writerow(['name', 'categories', 'average_rating', 'location/lat', 'location/lng', 'gPlusPlaceId'])
 
         # Ghi user_info vào tệp
         writer.writerow(user_info.values.flatten())
 
         # Ghi result vào tệp
         result.to_csv(file, header=False, index=False)
-    df_result_updated = pd.read_csv('result.csv')
+    df_result_updated = pd.read_csv('result_HCM1.csv')
     data_updated = df_result_updated.to_dict('records')
     return data_updated
 
@@ -251,27 +254,19 @@ def update_data(df_result,n_clicks, input_value):
         return df_result.to_dict('records')
 def update_map(n_clicks, data):
     df_result = pd.DataFrame(data).copy()
-    gps_data = df_result['GPS']
-    place_name_data = df_result['name']
-    df_location = pd.DataFrame({'GPS': gps_data, 'Place Name': place_name_data})
-    # Chia cột GPS thành cột Latitude và Longitude
-    df_location[['Latitude', 'Longitude']] = df_location['GPS'].str.extract('\[(.*?),\s(.*?)\]', expand=True)
-    # Chuyển đổi cột 'Longitude' và 'Latitude' sang kiểu dữ liệu số
-    df_location['Longitude'] = pd.to_numeric(df_location['Longitude'])
-    df_location['Latitude'] = pd.to_numeric(df_location['Latitude'])
     # Tạo cột 'Color' để xác định màu sắc cho các điểm
-    df_location['Color'] = 'others'  # Mặc định là màu đỏ cho tất cả các điểm
-    df_location.loc[0, 'Color'] = 'target'  # Điểm đầu tiên có màu xanh
+    df_result['Color'] = 'others'  # Mặc định là màu đỏ cho tất cả các điểm
+    df_result.loc[0, 'Color'] = 'target'  # Điểm đầu tiên có màu xanh
     # Định nghĩa bản đồ màu sắc
     color_discrete_map = {'target': '#f86134', 'others': '#002f72'}
     # Vẽ bản đồ bằng px.scatter_mapbox
     fig = px.scatter_mapbox(
-        df_location,
-        lat="Latitude",
-        lon="Longitude",
-        text="Place Name",
+        df_result,
+        lat="location/lat",
+        lon="location/lng",
+        text="name",
         mapbox_style='carto-positron',
-        hover_data={'Place Name': True},
+        hover_data={'name': True},
         color="Color",  # Sử dụng cột 'Color' để xác định màu sắc
         color_discrete_map=color_discrete_map  # Bản đồ màu sắc
     )
