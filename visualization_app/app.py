46d2fc657f15fdbc5ee78a4ba290adf3ac6bd692
NunNunIT <hongnhung02v04@gmail.com>
2024-02-07 00:58:59 +0700

    Add visualization app

---
+++ visualization_app/app.py
@@ -0,0 +1,230 @@
+import dash     # need Dash version 1.21.0 or higher
+from dash import dcc, html, Input, Output, State
+import dash_table
+
+import pandas as pd
+import plotly.express as px
+import pymongo
+from pymongo import MongoClient
+from bson import ObjectId
+from dashboard_layout import *
+from recomended_layout import *
+
+# Connect to local server
+client = MongoClient("mongodb://127.0.0.1:27017/")
+# Create database called animals
+mydb = client["animals"]
+# Create Collection (table) called shelterA
+collection = mydb.shelterA
+
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
+
+
+# Layout của Dash App
+app.layout = html.Div([
+    create_introduct_layout(),
+    html.H1("Dashboard", className="centered-heading", id="see-here"),
+    html.Div([
+        create_dashboard_option_layout(),
+        create_dashboard_total_layout(),
+        html.Div([
+                dcc.Graph(id='dashboard-review-chart'),
+                dcc.Graph(id='dashboarb-fig-star'),
+            ], className='contain-layout'),
+        html.Div(id='mongo-datatable', children=[]),
+        # dcc.Graph(id='dashboard-map'),
+    ], className="dashboard-container"),
+    html.H1("Categories", className="centered-heading"),
+    html.H1("Recomended System", className="centered-heading"),
+    create_PlaceName_PlaceID_Dropdown(),
+    create_SearchButton_layout(),
+    html.Div([
+        create_dashtable_result(),
+        dcc.Store(id='result-store', data=df_result.to_dict('records')),
+    ], className='contain-layout'),
+    html.H1("Sentiment", className="centered-heading"),
+
+    # activated once/week or when page refreshed
+    dcc.Interval(id='interval_db', interval=86400000 * 7, n_intervals=0),
+    html.Button("Save to Mongo Database", id="save-it"),
+    html.Button('Add Row', id='adding-rows-btn', n_clicks=0),
+], className='main-container')
+
+########### DASH BOARD ################################################################
+# Dashboard - 2. Callback to update dropdown options *************************
+@app.callback(
+    Output('place-dropdown', 'options'),
+    Input('interval_db', 'n_intervals'),
+)
+def update_dropdown(n_intervals):
+    dropdown_place_options = get_dropdown_options("placeId")
+
+    return dropdown_place_options
+
+# Dashboard - 1. Display Datatable with data from Mongo database *************************
+@app.callback([Output('mongo-datatable', 'children'),
+               Output('total-ratings', 'children'),
+               Output('total-reviews', 'children'),
+               Output('total-places', 'children'),
+               Output('total-user', 'children'),
+               Output('dashboard-review-chart', 'figure'),
+               Output('dashboarb-fig-star', 'figure'),],
+              [Input('interval_db', 'n_intervals'),
+               Input('year-dropdown', 'value'),
+               Input('place-dropdown', 'value')])
+def populate_datatable(n_intervals, select_year='All', select_place='All'):
+    # Truy vấn để lấy dữ liệu từ MongoDB
+    query = {}
+    
+    if select_year != 'All':
+        query["publishedAtDate"] = {"$regex": f"^{select_year}"}
+        
+    if select_place != 'All':
+        query["placeId"] = select_place
+
+    # Convert the Collection (table) date to a pandas DataFrame with applied filter
+    df = pd.DataFrame(list(collection.find(query)))
+    # Drop the _id column generated automatically by Mongo
+    df = df.iloc[:, 1:]
+    detail_table = [
+            dash_table.DataTable(
+                id='my-table',
+                columns=[{
+                    'name': x,
+                    'id': x,
+                } for x in df.columns],
+                data=df.to_dict('records'),
+                editable=True,
+                row_deletable=True,
+                filter_action="native",
+                filter_options={"case": "sensitive"},
+                sort_action="native",  # give user capability to sort columns
+                sort_mode="single",  # sort across 'multi' or 'single' columns
+                page_current=0,  # page number that user is on
+                page_size=10,  # number of rows visible per page
+                style_table={'height': '400px'},
+                style_cell={'maxWidth': 100, 'overflow': 'ellipsis', 'textOverflow': 'ellipsis', 'whiteSpace': 'nowrap'},
+                style_header={
+                    'backgroundColor': '#043296',
+                    'fontWeight': 'bold',
+                    'color': 'white',
+                    'textAlign': 'center'
+                },
+            )
+        ]
+    
+    # Đếm số lượng đánh giá (ratings), số lượng bình luận (reviews), số lượng địa điểm (places) và số lượng người dùng (users)
+    ratings = collection.count_documents(query)
+    
+    review_query = {"$and": [query, {"text": {"$exists": True}}]}
+    reviews = collection.count_documents(review_query)
+
+    places = len(collection.distinct("placeId", query))
+    users = len(collection.distinct("reviewerId", query))
+
+    # Draw graph
+    statistics_df = calculate_reviews_dashboarb(df)
+    fig_bar = draw_chart_reviews_dashboard(statistics_df)
+
+    fig_pie = draw_chart_reviews_by_star_dashboard(df)
+    
+    return [detail_table, ratings, reviews, places, users, fig_bar, fig_pie]
+
+# Dashboard - 2. Function to get dropdown options from MongoDB based on publishedAtDate *************************
+def get_dropdown_options(field_name):
+    distinct_values = collection.distinct(field_name) 
+    options = [
+        {'label': str(value), 'value': str(value)}
+        for value in distinct_values
+    ]
+    
+    options.insert(0, {'label': 'All', 'value': 'All'})
+    return options
+
+########## END OF DASHBOARD ################################################################################
+
+# Add new rows to DataTable ***********************************************
+@app.callback(
+    Output('my-table', 'data'),
+    [Input('adding-rows-btn', 'n_clicks')],
+    [State('my-table', 'data'),
+     State('my-table', 'columns')],
+)
+def add_row(n_clicks, rows, columns):
+    if n_clicks > 0:
+        rows.append({c['id']: '' for c in columns})
+    return rows
+
+
+# Save new DataTable data to the Mongo database ***************************
+@app.callback(
+    Output("placeholder", "children"),
+    Input("save-it", "n_clicks"),
+    State("my-table", "data"),
+    prevent_initial_call=True
+)
+def save_data(n_clicks, data):
+    dff = pd.DataFrame(data)
+    collection.delete_many({})
+    collection.insert_many(dff.to_dict('records'))
+    return ""
+
+########## BEGIN OF RECOMENDED SYSTEM ################################################################################
+# Callback để động cập nhật danh sách tùy chọn của dropdown PlaceID dựa trên giá trị được chọn trong dropdown placename
+@app.callback(
+    Output('dropdown-idplace', 'options'),
+    Input('dropdown-placename', 'value')
+)
+def update_idplace_options(selected_place_name):
+    # Lấy lại danh sách tên địa điểm và ID địa điểm từ MongoDB (có thể cần cập nhật nếu dữ liệu thay đổi)
+    place_data = collection.find({"name": selected_place_name}, {"gPlusPlaceId": 1})
+    updated_place_ids = [place["gPlusPlaceId"] for place in place_data]
+
+    # Tạo danh sách tùy chọn mới cho dropdown PlaceID
+    dropdown_options = [{'label': place_id, 'value': place_id} for place_id in updated_place_ids]
+
+    return dropdown_options
+
+@app.callback(
+    Output('result-store', 'data'),
+    [Input('button-get-value', 'n_clicks'),
+     Input('dropdown-idplace', 'value')],
+)
+
+def update_data_recommend(n_clicks, input_value):
+    if n_clicks > 0:
+        data_updated = recommendation(input_value)
+        return data_updated
+    else:
+        return df_result.to_dict('records')
+
+@app.callback(
+    Output('table_result', 'data'),
+    [Input('result-store', 'data')]
+)
+def update_table(data):
+    return data
+
+@app.callback(
+    Output('map-result', 'figure'),
+    [Input('button-get-value', 'n_clicks')],
+    [Input('table_result', 'data')]
+)
+def update_map_recommendation(n_clicks, data):
+    fig = update_map(n_clicks,data)
+    return fig
+
+# Define callback to update links when table_result changes
+@app.callback(
+    Output('link-container', 'children'),
+    [Input('table_result', 'data')]
+)
+def update_links(data):
+    print("Callback update_links is running.")
+    # Extract the 'url' column from the table data
+    urls = [row.get('url', '') for row in data]
+    links = [html.A(href=url, target='_blank', children="Xem", className='button2') for url in urls]
+    return links
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ visualization_app/recomended_layout.py
@@ -0,0 +1,228 @@
+from dash import Dash, html, dcc, callback, Output, Input, State
+from dash import dash_table
+import plotly.express as px
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import csv
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics.pairwise import haversine_distances
+from math import radians
+from haversine import haversine, Unit
+from pymongo import MongoClient
+
+# Connect to local server
+client = MongoClient("mongodb://127.0.0.1:27017/")
+# Create database called animals
+mydbkha = client["animals"]
+# Create Collectionkha (table) called shelterA
+collectionkha = mydbkha.RecommendData
+# Lấy danh sách tên địa điểm và ID địa điểm từ MongoDB
+place_data = collectionkha.find({}, {"name": 1, "gPlusPlaceId": 1})
+place_names = [place["name"] for place in place_data]
+place_ids = [place["gPlusPlaceId"] for place in place_data]
+
+
+df_result = pd.read_csv('result_HCM1.csv',encoding='utf-8')
+
+def create_PlaceName_PlaceID_Dropdown():
+    return html.Div([
+            html.Div([
+                html.Label('Place Name'),
+                dcc.Dropdown(
+                    id='dropdown-placename',
+                    options=[{'label': place_name, 'value': place_name} for place_name in place_names],
+                    value='All',
+                ),
+                ], className='dropdown-item'),
+            html.Div([
+                html.Label('ID Place'),
+                dcc.Dropdown(
+                    id='dropdown-idplace',
+                    options=[{'label': place_id, 'value': place_id} for place_id in place_ids],
+                    value='All',
+                ),
+            ], className='dropdown-item')
+        ], className='contain-layout')
+def create_dashtable_result():
+    df_result_filtered = df_result.drop(columns=["gPlusPlaceId", "url"])
+    df_result_filtered = df_result_filtered.drop(df_result_filtered.index[0])
+
+    # Tạo một danh sách chứa thẻ <a> tương ứng với từng giá trị trong cột 'url'
+    links = [html.A(href=url, target='_blank', children="Xem", className='button2') for url in df_result['url']]
+
+    return html.Div([
+        html.Div([
+            dcc.Graph(id='map-result', config={'displayModeBar': False})
+        ]),
+        html.Div([
+            dash_table.DataTable(
+                id='table_result',
+                columns=[
+                    {'name': col, 'id': col} for col in df_result_filtered.columns
+                ],
+                data=df_result_filtered.to_dict('records'),
+                style_table={'height': '400px', 'width': '100%', 'minWidth': '100%'},
+                style_cell={'maxWidth': 100, 'overflow': 'ellipsis', 'textOverflow': 'ellipsis',
+                            'whiteSpace': 'nowrap'},
+                style_header={
+                    'backgroundColor': '#043296',
+                    'fontWeight': 'bold',
+                    'color': 'white',
+                    'textAlign': 'center'
+                },
+            ),
+            html.Div(links, id='link-container', className="link-container1"),
+        ], className='contain-layout2'),
+    ], className='contain-layout')
+def create_SearchButton_layout():
+    return html.Div([
+            html.Button("Search", id='button-get-value', n_clicks=0,
+                        style={'font-size': '18px', 'border': '2px solid #015fc2', 'padding': '5px 10px',
+                               'background-color': '#015fc2', 'color': 'white', 'display': 'inline-block',
+                               'border-radius': '5px', 'padding': '10px'}),
+        ], style={'margin-right': '20px', 'margin-top': '10px', 'text-align': 'center'})
+
+
+
+def update_graph(df):
+    title = 'Number of reviews'
+    dff = df.copy()
+    histogram_figure = px.histogram(dff, x='rating', title=title,
+                                    labels={'x': 'Rating', 'y': 'Số lượng đánh giá'},
+                                    color_discrete_sequence=['#043296'],
+                                    category_orders=dict(rating=["1", "2", "3", "4", "5"]))
+    df2 = dff.copy()
+
+    map_figure = px.scatter_mapbox(df2,
+                                   lat='location/lat',
+                                   lon='location/lng',
+                                   text='name',
+                                   mapbox_style='carto-positron',
+                                   color_discrete_sequence=['#002f72'])
+    map_figure.update_traces(
+        marker=dict(
+            size=10
+        ),
+        textposition='top center'
+    )
+    map_figure.update_layout(
+        mapbox=dict(
+            style='carto-positron',
+            zoom=4
+        ),
+        autosize=True,
+        margin=dict(l=0, r=0, t=0, b=0),
+    )
+
+    return histogram_figure, map_figure
+
+# Đọc dữ liệu từ MongoDB và xây dựng mô hình ở đây
+cursor = collectionkha.find({})
+data = pd.DataFrame(list(cursor))
+
+# Tiền xử lý dữ liệu
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf_vectorizer.fit_transform(data['categories'].fillna(''))
+
+# Xây dựng mô hình
+model = NearestNeighbors(n_neighbors=6, algorithm='ball_tree')
+model.fit(tfidf_matrix)
+
+# Hàm khuyến nghị
+def recommend(gPlusPlaceId):
+    # Lấy thông tin từ dataset dựa trên gPlusPlaceId
+    user_info = data[data['gPlusPlaceId'] == gPlusPlaceId][['name', 'categories', 'average_rating', 'location/lat',
+                                                          'location/lng', 'gPlusPlaceId','url']]
+
+    # Lấy vị trí từ dataset dựa trên gPlusPlaceId
+    input_lat = data[data['gPlusPlaceId'] == gPlusPlaceId]['location/lat'].iloc[0]
+    input_lng = data[data['gPlusPlaceId'] == gPlusPlaceId]['location/lng'].iloc[0]
+
+    input_coords = [input_lat, input_lng]
+
+    idx = data[data['gPlusPlaceId'] == gPlusPlaceId].index[0]
+    distances, indices = model.kneighbors(tfidf_matrix[idx])
+    recommended_places = data.loc[indices[0][1:]]
+
+    # Kiểm tra khoảng cách giữa vị trí nhập vào và vị trí của các địa điểm được khuyến nghị
+    input_coords_radians = [radians(coord) for coord in input_coords]
+    recommended_places['distance'] = recommended_places.apply(
+        lambda row: haversine(input_coords_radians, [radians(row['location/lat']), radians(row['location/lng'])],
+                              unit=Unit.KILOMETERS), axis=1)
+    recommended_places = recommended_places.sort_values(by='distance').drop(columns=['distance'])
+
+    # Loại bỏ gPlusPlaceId nhập vào khỏi danh sách khuyến nghị
+    recommended_places = recommended_places[recommended_places['gPlusPlaceId'] != gPlusPlaceId]
+
+    # Sắp xếp theo average_rating giảm dần
+    recommended_places = recommended_places.sort_values(by='average_rating', ascending=False)
+
+    return user_info, recommended_places[['name','categories','average_rating','location/lat','location/lng','gPlusPlaceId','url']]
+
+# average_ratings = df.groupby('gPlusPlaceId')['average_rating'].mean().reset_index()
+# merged_data = pd.merge(average_ratings, df[['gPlusPlaceId', 'gps', 'categories', 'name']], on='gPlusPlaceId', how='left')
+# df1 = merged_data.drop_duplicates(subset='gPlusPlaceId', keep='first')
+
+def recommendation(placeid):
+    user_info, result = recommend(placeid)
+    # Ghi thông tin của user_input và kết quả vào cùng một tệp CSV
+    with open('result_HCM1.csv', 'w', newline='', encoding='utf-8-sig') as file:
+        writer = csv.writer(file)
+
+        # Ghi header
+        writer.writerow(['name', 'categories', 'average_rating', 'location/lat', 'location/lng', 'gPlusPlaceId','url'])
+
+        # Ghi user_info vào tệp
+        writer.writerow(user_info.values.flatten())
+
+        # Ghi result vào tệp
+        result.to_csv(file, header=False, index=False)
+    df_result_updated = pd.read_csv('result_HCM1.csv')
+    data_updated = df_result_updated.to_dict('records')
+    return data_updated
+
+
+def update_data(df_result,n_clicks, input_value):
+    if n_clicks > 0:
+        print("Xin vui lòng đợi...")
+        data_updated = recommendation(input_value)
+        print("Success")
+        return data_updated
+    else:
+        return df_result.to_dict('records')
+def update_map(n_clicks, data):
+    df_result = pd.DataFrame(data).copy()
+    # Tạo cột 'Color' để xác định màu sắc cho các điểm
+    df_result['Color'] = 'others'  # Mặc định là màu đỏ cho tất cả các điểm
+    df_result.loc[0, 'Color'] = 'target'  # Điểm đầu tiên có màu xanh
+    # Định nghĩa bản đồ màu sắc
+    color_discrete_map = {'target': '#f86134', 'others': '#002f72'}
+    # Vẽ bản đồ bằng px.scatter_mapbox
+    fig = px.scatter_mapbox(
+        df_result,
+        lat="location/lat",
+        lon="location/lng",
+        text="name",
+        mapbox_style='carto-positron',
+        hover_data={'name': True},
+        color="Color",  # Sử dụng cột 'Color' để xác định màu sắc
+        color_discrete_map=color_discrete_map  # Bản đồ màu sắc
+    )
+    # Cấu hình layout cho bản đồ
+    fig.update_traces(
+        marker=dict(
+            size=10
+        ),
+        textposition='top center'
+    )
+    fig.update_layout(
+        mapbox=dict(
+            style='carto-positron',
+            zoom=4
+        ),
+        autosize=True,
+        margin=dict(l=0, r=0, t=0, b=0),
+    )
+    return fig
