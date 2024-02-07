96d33a9e86ba2699b24b1f4018c2b41823ae5506
NunNunIT <hongnhung02v04@gmail.com>
2024-02-07 20:13:54 +0700

    Finish sentiments layout

--- visualization_app/app.py
+++ visualization_app/app.py
@@ -1,14 +1,14 @@
 import dash     # need Dash version 1.21.0 or higher
 from dash import dcc, html, Input, Output, State
 import dash_table
-
 import pandas as pd
-import plotly.express as px
-import pymongo
 from pymongo import MongoClient
-from bson import ObjectId
+import plotly.express as px
+# from bson import ObjectId
 from dashboard_layout import *
 from recomended_layout import *
+from sentiments_layout import *
+from gensim.utils import simple_preprocess
 
 # Connect to local server
 client = MongoClient("mongodb://127.0.0.1:27017/")
@@ -38,7 +38,6 @@ app.layout = html.Div([
         html.Div(id='mongo-datatable', children=[]),
         # dcc.Graph(id='dashboard-map'),
     ], className="dashboard-container"),
-    html.H1("Categories", className="centered-heading"),
     html.H1("Recomended System", className="centered-heading"),
     html.Div([
         create_PlaceName_PlaceID_Dropdown(),
@@ -50,9 +49,17 @@ app.layout = html.Div([
     ], className="dashboard-container"),
     html.H1("Sentiment", className="centered-heading"),
     html.Div([
+        create_sentiment_option_layout(),
+        html.Div([
+                dcc.Graph(id='sentiment-positive-chart'),
+                dcc.Graph(id='sentiment-negative-chart'),
+            ], className='contain-layout'),
+    ], className="dashboard-container"),
+    html.Div([
     ], className="dashboard-container"),
     # activated once/week or when page refreshed
-    dcc.Interval(id='interval_db', interval=86400000 * 7, n_intervals=0),
+    # dcc.Interval(id='interval_db', interval=86400000 * 7, n_intervals=0),
+    dcc.Interval(id='interval_db', interval=10*1000, n_intervals=0),
     html.Button("Save to Mongo Database", id="save-it"),
     html.Button('Add Row', id='adding-rows-btn', n_clicks=0),
 ], className='main-container')
@@ -84,17 +91,17 @@ def get_dropdown_options(field_name):
         {'label': str(value), 'value': str(value)}
         for value in distinct_values
     ]
-    
     return options
 
 @app.callback(
-    Output('place-dropdown', 'options'),
+    [Output('place-dropdown', 'options'),
+     Output('place-sentiment-dropdown', 'options')],
     Input('interval_db', 'n_intervals'),
 )
 def update_dropdown(n_intervals):
     dropdown_place_options = get_dropdown_options("placeId")
 
-    return dropdown_place_options
+    return [dropdown_place_options, dropdown_place_options]
 
 # Dashboard - 2. Display with data from Mongo database *************************
 @app.callback([Output('mongo-datatable', 'children'),
@@ -131,7 +138,6 @@ def dashboard_display(select_year='All', select_place='All'):
     
     # Drop the _id column generated automatically by Mongo
     df.drop('_id', axis=1, inplace=True)
-    print(df.head(20))
     detail_table = [
             dash_table.DataTable(
                 id='my-table',
@@ -250,7 +256,6 @@ def update_map_recommendation(n_clicks, data):
     [Input('table_result', 'data')]
 )
 def update_links(data):
-    print("Callback update_links is running.")
     # Extract the 'url' column from the table data
     urls = [row.get('url', '') for row in data]
     links = [html.A(href=url, target='_blank', children="Xem", className='button2') for url in urls]
@@ -258,7 +263,44 @@ def update_links(data):
 
 ########## END OF RECOMENDED SYSTEM ################################################################################
 
+########## BEGIN OF SENTIMENTS ################################################################################
+@app.callback([Output('sentiment-positive-chart', 'figure'),
+               Output('sentiment-negative-chart', 'figure')],
+              [Input('year-sentiment-dropdown', 'value'),
+               Input('place-sentiment-dropdown', 'value')])
 
+def update_sentiments(select_year='All', select_place='All'):
+    # Khởi tạo danh sách rỗng cho sentences và sentiment
+    sentences = []
+    sentiment = []
+    query = {}
+
+    if select_year != 'All':
+        query["publishedAtDate"] = {"$regex": f"^{select_year}"}
+        
+    if select_place != 'All':
+        query["placeId"] = select_place
+
+    data_from_mongo = collection_reviews.find(query, {"sentences": 1, "sentiment": 1})
+
+    for data in data_from_mongo:
+        sentences.extend(data["sentences"])  # Sử dụng extend để thêm tất cả các phần tử của list vào danh sách sentences
+        sentiment.extend(data["sentiment"])
+
+    # Lọc các câu có sentiment là "negative"
+    negative_sentences = [sentences[i] for i in range(len(sentences)) if sentiment[i] == "negative"]
+    positive_sentences = [sentences[i] for i in range(len(sentences)) if sentiment[i] == "positive"]
+
+    # Tiền xử lý văn bản
+    cleaned_negative_sentences = [' '.join(simple_preprocess(sentence, min_len=2, max_len=15)) for sentence in negative_sentences]
+    cleaned_positive_sentences = [' '.join(simple_preprocess(sentence, min_len=2, max_len=15)) for sentence in positive_sentences]
+
+
+    fig_negative = create_fig_sentiments(cleaned_negative_sentences, 'lightsalmon')
+    fig_positive = create_fig_sentiments(cleaned_positive_sentences, 'lightsalmon')
+
+    return [fig_positive, fig_negative]
+########## BEGIN OF SENTIMENTS ################################################################################
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ visualization_app/sentiments_layout.py
@@ -0,0 +1,70 @@
+from dash import html, dash_table
+from dash import Dash, html, dcc, callback, Output, Input
+from wordcloud import WordCloud
+from collections import Counter
+import matplotlib.pyplot as plt
+from matplotlib import colormaps
+import plotly.graph_objects as go
+import nltk
+
+def create_sentiment_option_layout():
+    return html.Div([
+                html.Div([
+                    html.Label('Year'),
+                    dcc.Dropdown(
+                        id='year-sentiment-dropdown',
+                        options= [{'label': 'All', 'value': 'All'}] + [{'label': str(year), 'value': str(year)} for year in range(1990, 2025)],
+                        value= 'All',  # Giá trị mặc định là All
+                        placeholder='Chọn năm',  # Nhãn placeholder
+                    ),
+                ], className = 'dropdown-item'),
+                html.Div([
+                    html.Label('Places'),
+                    dcc.Dropdown(
+                        id='place-sentiment-dropdown',
+                        options= [{'label': 'ChIJp67U2gspdTERdMFp0ZUhXT4', 'value': 'ChIJp67U2gspdTERdMFp0ZUhXT4'}],
+                        value= 'ChIJp67U2gspdTERdMFp0ZUhXT4',
+                        placeholder='Chọn địa điểm',  # Nhãn placeholder
+                    ),
+                ], className = 'dropdown-item'),
+            ], className='contain-layout')
+
+# Tạo danh sách stopwords (đã có từ trước)
+stop_words = set()
+# Đọc từng dòng trong file stopwords.txt và thêm từ vào set stop_words
+with open("vietnamese-stopwords.txt", "r", encoding="utf-8") as file:
+    for line in file:
+        word = line.strip()  # Loại bỏ khoảng trắng và ký tự xuống dòng
+        stop_words.add(word)
+
+# Định nghĩa hàm combine_negation
+def combine_negation(tokens):
+    negation_words = {"không", "ko", "k", "kh", "không nên", "k nên", "ko nên", "chưa", "có"}
+    combined_tokens = []
+    i = 0
+    while i < len(tokens):
+        if i > 0 and i < len(tokens) - 1:
+            combined_tokens.append(tokens[i - 1] + "_" + tokens[i] + "_" + tokens[i + 1])
+            combined_tokens.append(tokens[i])
+        i += 1
+    return combined_tokens
+
+
+def create_fig_sentiments(data_list, color_c):
+    data_list_combined = []
+    for sentence in data_list:
+        tokens = nltk.word_tokenize(sentence)
+        tokens = [token.lower() for token in tokens if token.lower() not in stop_words]  # Loại bỏ stop words
+        combined_tokens = combine_negation(tokens)
+        data_list_combined.extend(combined_tokens)
+    
+    # Đếm tần suất xuất hiện của các từ
+    word_freq = Counter(data_list_combined)
+    most_common_words = word_freq.most_common(20)  # Lấy 20 từ xuất hiện nhiều nhất
+    
+    # Tạo biểu đồ cột
+    words, frequencies = zip(*most_common_words)
+    fig = go.Figure(data=[go.Bar(x=words, y=frequencies, text=frequencies,
+            textposition='auto', marker_color=color_c)]) # Chú ý marker_color ở đây
+    fig.update_layout(title_text='Most Common Words', xaxis_title="Words", yaxis_title="Frequency")
+    return fig
